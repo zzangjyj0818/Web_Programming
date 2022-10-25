ebdd49d1b97fe64298fbbb0c85adeab18b3649e9
Jeong Yeon Jae <jyj010818@naver.com>
2022-10-25 16:37:48 +0900

    Web Programming

---
+++ lecture01.js
@@ -0,0 +1,3 @@
+alert("Hello JavaScript!");
+// alert => 알림창.
+//Hello JavaScript를 알림으로 출력
---
+++ lecture02.js
@@ -0,0 +1,5 @@
+console.log("Hello JavaScript");
+console.log("2");
+console.log("3");
+
+//console.log는 개발자 도구에서 볼 수 있음.
