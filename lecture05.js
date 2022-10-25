10544d8fd2c6f6c7420c66967be0a0f0ffe03a34
Jeong Yeon Jae <jyj010818@naver.com>
2022-10-25 16:46:36 +0900

    Web Programming

---
+++ lecture05.js
@@ -0,0 +1,12 @@
+//Boolean 자료형
+// True False를 다루는 자료형
+
+//typeof(변수) : 괄호 안에 들어간 변수의 자료형이 무엇인지 알려주는 함수
+
+var a = 100; //int
+var b = 3.14; //float
+var c = "hello"; //string
+var d = "a"; //char
+var e = true, d = false; //boolean
+
+console.log(a,typeof(a));
